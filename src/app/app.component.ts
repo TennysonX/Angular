6252bdc22bdb772d2559ca8c185231397995679d
TennysonX <teerapakorn.meesri@gmail.com>
2024-07-13 15:22:52 +0700

    For Switch case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -11,11 +12,18 @@ export class AppComponent {
   title = 'Todo_List';
   imageCoverUrl = '/images/todo-list.png'
   onimageClick(){
-    alert("ใครกดเป็นเกย์")
+    alert("ใ ค ร ก ด เ ป็ น เ ก ย์")
   }
   major = "programmer"
+
   items = [
     "A","B","C","D","E"
   ]
   size = "M"
+
+  sizelist = [
+    "S","M","M","XL",'XXL','XXXL'
+  ]
+
+  now = new Date()
 }
