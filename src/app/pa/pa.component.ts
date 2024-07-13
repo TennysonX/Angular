4e9d08315e326fc543fa6cd836417f8f21f11607
TennysonX <teerapakorn.meesri@gmail.com>
2024-07-13 11:33:54 +0700

    commit

---
+++ src/app/pa/pa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pa',
+  standalone: true,
+  imports: [],
+  templateUrl: './pa.component.html',
+  styleUrl: './pa.component.css'
+})
+export class PaComponent {
+
+}
